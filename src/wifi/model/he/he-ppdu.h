nsnam/ns-3-dev-git

wifi: Fix amount of users per content channel and user infos in reconstructed TXVECTOR for HE/EHT MU PPDUs

src/wifi/model/he/he-ppdu.h
@@ -187,6 +187,7 @@ class HePpdu : public OfdmPpdu
      *
      * @param channelWidth the channel width occupied by the PPDU
      * @param ruAllocation 8 bit RU_ALLOCATION per 20 MHz
+     * @param center26ToneRuIndication the center 26 tone RU indication
      * @param sigBCompression flag whether SIG-B compression is used by the PPDU
      * @param numMuMimoUsers the number of MU-MIMO users addressed by the PPDU
      * @return a pair containing the number of RUs in each HE-SIG-B content channel (resp. 1 and 2)
@@ -194,6 +195,7 @@ class HePpdu : public OfdmPpdu
     static std::pair<std::size_t, std::size_t> GetNumRusPerHeSigBContentChannel(
         MHz_t channelWidth,
         const RuAllocation& ruAllocation,
+        std::optional<Center26ToneRuIndication> center26ToneRuIndication,
         bool sigBCompression,
         uint8_t numMuMimoUsers);
 
@@ -212,14 +214,17 @@ class HePpdu : public OfdmPpdu
      * Get variable length HE SIG-B field size
      * @param channelWidth the channel width occupied by the PPDU
      * @param ruAllocation 8 bit RU_ALLOCATION per 20 MHz
+     * @param center26ToneRuIndication the center 26 tone RU indication
      * @param sigBCompression flag whether SIG-B compression is used by the PPDU
      * @param numMuMimoUsers the number of MU-MIMO users addressed by the PPDU
      * @return field size in bytes
      */
-    static uint32_t GetSigBFieldSize(MHz_t channelWidth,
-                                     const RuAllocation& ruAllocation,
-                                     bool sigBCompression,
-                                     std::size_t numMuMimoUsers);
+    static uint32_t GetSigBFieldSize(
+        MHz_t channelWidth,
+        const RuAllocation& ruAllocation,
+        std::optional<Center26ToneRuIndication> center26ToneRuIndication,
+        bool sigBCompression,
+        std::size_t numMuMimoUsers);
 
   protected:
     /**
@@ -241,15 +246,33 @@ class HePpdu : public OfdmPpdu
      *
      * @param txVector the TXVECTOR to set its HeMuUserInfoMap
      * @param ruAllocation the RU_ALLOCATION per 20 MHz
+     * @param center26ToneRuIndication the center 26 tone RU indication
      * @param contentChannels the HE-SIG-B content channels
      * @param sigBCompression flag whether SIG-B compression is used by the PPDU
      * @param numMuMimoUsers the number of MU-MIMO users addressed by the PPDU
      */
     void SetHeMuUserInfos(WifiTxVector& txVector,
                           const RuAllocation& ruAllocation,
+                          std::optional<Center26ToneRuIndication> center26ToneRuIndication,
                           const HeSigBContentChannels& contentChannels,
                           bool sigBCompression,
                           uint8_t numMuMimoUsers) const;
+
+    /**
+     * Get the RU specification that has been assigned a given user.
+     *
+     * @param ruAllocIndex the index of the RU allocation
+     * @param ruSpecs RU specs deduced from the RU allocation
+     * @param ruType RU type of the user to be assigned
+     * @param ruIndex RU index of the user to be assigned
+     * @param bw the total bandwidth used for the transmission
+     * @return the value used to encode the bandwidth field in HE-SIG-A
+     */
+    virtual HeRu::RuSpec GetRuSpec(std::size_t ruAllocIndex,
+                                   const std::vector<HeRu::RuSpec>& ruSpecs,
+                                   HeRu::RuType ruType,
+                                   std::size_t ruIndex,
+                                   MHz_t bw) const;
 
     /**
      * Convert channel width expressed in MHz to bandwidth field encoding in HE-SIG-A.

src/wifi/model/wifi-tx-vector.cc
@@ -607,12 +607,6 @@ WifiTxVector::GetNumStasInRu(const HeRu::RuSpec& ru) const
                          [&ru](const auto& info) -> bool { return (ru == info.second.ru); });
 }
 
-bool
-WifiTxVector::IsAllocated(uint16_t staId) const
-{
-    return m_muUserInfos.contains(staId);
-}
-
 HeRu::RuSpec
 WifiTxVector::GetRu(uint16_t staId) const
 {
@@ -810,30 +804,39 @@ WifiTxVector::DeriveRuAllocation(uint8_t p20Index) const
     std::vector<HeRu::RuType> ruTypes{};
     ruTypes.resize(ruAllocations.size());
     const auto& orderedMap = GetUserInfoMapOrderedByRus(p20Index);
+    std::pair<std::size_t /* number of RUs in content channel 1 */,
+              std::size_t /* number of RUs in content channel 2 */>
+        ccSizes{0, 0};
     for (const auto& [ru, staIds] : orderedMap)
     {
+        if ((ru.GetRuType() == HeRu::RU_26_TONE) && (ru.GetIndex() == 19))
+        {
+            continue;
+        }
         const auto ruType = ru.GetRuType();
         const auto ruBw = HeRu::GetBandwidth(ruType);
-        const auto isPrimary80MHz = ru.GetPrimary80MHz();
         const auto rusPerSubchannel =
             HeRu::GetRusOfType(ruBw > MHz_t{20} ? ruBw : MHz_t{20}, ruType);
         auto ruIndex = ru.GetIndex();
         if ((m_channelWidth >= MHz_t{80}) && (ruIndex > 19))
         {
-            // take into account the center 26-tone RU in the primary 80 MHz
+            // take into account the center 26-tone RU in the low 80 MHz
             ruIndex--;
         }
-        if ((!isPrimary80MHz) && (ruIndex > 19))
+        const auto isPrimary80MHz = ru.GetPrimary80MHz();
+        const auto primary80IsLower80 = (p20Index < m_channelWidth / MHz_t{40});
+        const auto isLow80 =
+            (primary80IsLower80 && isPrimary80MHz) || (!primary80IsLower80 && !isPrimary80MHz);
+        if ((m_channelWidth > MHz_t{80}) && !isLow80 && (ruIndex > 19))
         {
-            // take into account the center 26-tone RU in the secondary 80 MHz
+            // take into account the center 26-tone RU in the high 80 MHz
             ruIndex--;
         }
-        if (!isPrimary80MHz && (ruType != HeRu::RU_2x996_TONE))
+        if ((m_channelWidth > MHz_t{80}) && !isLow80 && (ruType != HeRu::GetRuType(m_channelWidth)))
         {
-            NS_ASSERT(m_channelWidth > MHz_t{80});
-            // adjust RU index for the secondary 80 MHz: in that case index is restarting at 1,
+            // adjust RU index for the high 80 MHz: in that case index is restarting at 1,
             // hence we need to add an offset corresponding to the number of RUs of the same type in
-            // the primary 80 MHz
+            // the low 80 MHz
             ruIndex += HeRu::GetRusOfType(MHz_t{80}, ruType).size();
         }
         const auto numSubchannelsForRu = (ruBw < MHz_t{20}) ? 1 : Count20MHzSubchannels(ruBw);
@@ -860,10 +863,29 @@ WifiTxVector::DeriveRuAllocation(uint8_t p20Index) const
                 NS_ASSERT_MSG(false, "unsupported RU combination");
             }
         }
+        std::size_t ccIndex;
+        if (ruType >= HeRu::RU_484_TONE)
+        {
+            ccIndex = (ccSizes.first <= ccSizes.second) ? 0 : 1;
+        }
+        else
+        {
+            ccIndex = (index % 2 == 0) ? 0 : 1;
+        }
+        if (ccIndex == 0)
+        {
+            ccSizes.first += staIds.size();
+        }
+        else
+        {
+            ccSizes.second += staIds.size();
+        }
         for (std::size_t i = 0; i < numSubchannelsForRu; ++i)
         {
+            auto ruAllocNoUsers = HeRu::GetEqualizedRuAllocation(ruType, false, false);
             ruTypes.at(index + i) = ruType;
-            ruAllocations.at(index + i) = ruAlloc;
+            ruAllocations.at(index + i) =
+                (IsSigBCompression() || ((index + i) % 2) == ccIndex) ? ruAlloc : ruAllocNoUsers;
         }
     }
     return ruAllocations;

sksayed/ns3_project_for_kit

src/wifi/model/he/he-ppdu.h
@@ -184,6 +184,7 @@ class HePpdu : public OfdmPpdu
      *
      * @param channelWidth the channel width occupied by the PPDU
      * @param ruAllocation 8 bit RU_ALLOCATION per 20 MHz
+     * @param center26ToneRuIndication the center 26 tone RU indication
      * @param sigBCompression flag whether SIG-B compression is used by the PPDU
      * @param numMuMimoUsers the number of MU-MIMO users addressed by the PPDU
      * @return a pair containing the number of RUs in each HE-SIG-B content channel (resp. 1 and 2)
@@ -191,6 +192,7 @@ class HePpdu : public OfdmPpdu
     static std::pair<std::size_t, std::size_t> GetNumRusPerHeSigBContentChannel(
         MHz_u channelWidth,
         const RuAllocation& ruAllocation,
+        std::optional<Center26ToneRuIndication> center26ToneRuIndication,
         bool sigBCompression,
         uint8_t numMuMimoUsers);
 
@@ -209,14 +211,17 @@ class HePpdu : public OfdmPpdu
      * Get variable length HE SIG-B field size
      * @param channelWidth the channel width occupied by the PPDU
      * @param ruAllocation 8 bit RU_ALLOCATION per 20 MHz
+     * @param center26ToneRuIndication the center 26 tone RU indication
      * @param sigBCompression flag whether SIG-B compression is used by the PPDU
      * @param numMuMimoUsers the number of MU-MIMO users addressed by the PPDU
      * @return field size in bytes
      */
-    static uint32_t GetSigBFieldSize(MHz_u channelWidth,
-                                     const RuAllocation& ruAllocation,
-                                     bool sigBCompression,
-                                     std::size_t numMuMimoUsers);
+    static uint32_t GetSigBFieldSize(
+        MHz_u channelWidth,
+        const RuAllocation& ruAllocation,
+        std::optional<Center26ToneRuIndication> center26ToneRuIndication,
+        bool sigBCompression,
+        std::size_t numMuMimoUsers);
 
   protected:
     /**
@@ -231,15 +236,33 @@ class HePpdu : public OfdmPpdu
      *
      * @param txVector the TXVECTOR to set its HeMuUserInfoMap
      * @param ruAllocation the RU_ALLOCATION per 20 MHz
+     * @param center26ToneRuIndication the center 26 tone RU indication
      * @param contentChannels the HE-SIG-B content channels
      * @param sigBCompression flag whether SIG-B compression is used by the PPDU
      * @param numMuMimoUsers the number of MU-MIMO users addressed by the PPDU
      */
     void SetHeMuUserInfos(WifiTxVector& txVector,
                           const RuAllocation& ruAllocation,
+                          std::optional<Center26ToneRuIndication> center26ToneRuIndication,
                           const HeSigBContentChannels& contentChannels,
                           bool sigBCompression,
                           uint8_t numMuMimoUsers) const;
+
+    /**
+     * Get the RU specification that has been assigned a given user.
+     *
+     * @param ruAllocIndex the index of the RU allocation
+     * @param ruSpecs RU specs deduced from the RU allocation
+     * @param ruType RU type of the user to be assigned
+     * @param ruIndex RU index of the user to be assigned
+     * @param bw the total bandwidth used for the transmission
+     * @return the value used to encode the bandwidth field in HE-SIG-A
+     */
+    virtual HeRu::RuSpec GetRuSpec(std::size_t ruAllocIndex,
+                                   const std::vector<HeRu::RuSpec>& ruSpecs,
+                                   HeRu::RuType ruType,
+                                   std::size_t ruIndex,
+                                   MHz_u bw) const;
 
     /**
      * Convert channel width expressed in MHz to bandwidth field encoding in HE-SIG-A.

src/wifi/model/wifi-tx-vector.cc
@@ -595,12 +595,6 @@ WifiTxVector::GetNumStasInRu(const HeRu::RuSpec& ru) const
                          [&ru](const auto& info) -> bool { return (ru == info.second.ru); });
 }
 
-bool
-WifiTxVector::IsAllocated(uint16_t staId) const
-{
-    return m_muUserInfos.contains(staId);
-}
-
 HeRu::RuSpec
 WifiTxVector::GetRu(uint16_t staId) const
 {
@@ -798,30 +792,39 @@ WifiTxVector::DeriveRuAllocation(uint8_t p20Index) const
     std::vector<HeRu::RuType> ruTypes{};
     ruTypes.resize(ruAllocations.size());
     const auto& orderedMap = GetUserInfoMapOrderedByRus(p20Index);
+    std::pair<std::size_t /* number of RUs in content channel 1 */,
+              std::size_t /* number of RUs in content channel 2 */>
+        ccSizes{0, 0};
     for (const auto& [ru, staIds] : orderedMap)
     {
+        if ((ru.GetRuType() == HeRu::RU_26_TONE) && (ru.GetIndex() == 19))
+        {
+            continue;
+        }
         const auto ruType = ru.GetRuType();
         const auto ruBw = HeRu::GetBandwidth(ruType);
-        const auto isPrimary80MHz = ru.GetPrimary80MHz();
         const auto rusPerSubchannel =
             HeRu::GetRusOfType(ruBw > MHz_u{20} ? ruBw : MHz_u{20}, ruType);
         auto ruIndex = ru.GetIndex();
         if ((m_channelWidth >= MHz_u{80}) && (ruIndex > 19))
         {
-            // take into account the center 26-tone RU in the primary 80 MHz
+            // take into account the center 26-tone RU in the low 80 MHz
             ruIndex--;
         }
-        if ((!isPrimary80MHz) && (ruIndex > 19))
+        const auto isPrimary80MHz = ru.GetPrimary80MHz();
+        const auto primary80IsLower80 = (p20Index < m_channelWidth / MHz_u{40});
+        const auto isLow80 =
+            (primary80IsLower80 && isPrimary80MHz) || (!primary80IsLower80 && !isPrimary80MHz);
+        if ((m_channelWidth > MHz_u{80}) && !isLow80 && (ruIndex > 19))
         {
-            // take into account the center 26-tone RU in the secondary 80 MHz
+            // take into account the center 26-tone RU in the high 80 MHz
             ruIndex--;
         }
-        if (!isPrimary80MHz && (ruType != HeRu::RU_2x996_TONE))
+        if ((m_channelWidth > MHz_u{80}) && !isLow80 && (ruType != HeRu::GetRuType(m_channelWidth)))
         {
-            NS_ASSERT(m_channelWidth > MHz_u{80});
-            // adjust RU index for the secondary 80 MHz: in that case index is restarting at 1,
+            // adjust RU index for the high 80 MHz: in that case index is restarting at 1,
             // hence we need to add an offset corresponding to the number of RUs of the same type in
-            // the primary 80 MHz
+            // the low 80 MHz
             ruIndex += HeRu::GetRusOfType(MHz_u{80}, ruType).size();
         }
         const auto numSubchannelsForRu = (ruBw < MHz_u{20}) ? 1 : Count20MHzSubchannels(ruBw);
@@ -848,10 +851,29 @@ WifiTxVector::DeriveRuAllocation(uint8_t p20Index) const
                 NS_ASSERT_MSG(false, "unsupported RU combination");
             }
         }
+        std::size_t ccIndex;
+        if (ruType >= HeRu::RU_484_TONE)
+        {
+            ccIndex = (ccSizes.first <= ccSizes.second) ? 0 : 1;
+        }
+        else
+        {
+            ccIndex = (index % 2 == 0) ? 0 : 1;
+        }
+        if (ccIndex == 0)
+        {
+            ccSizes.first += staIds.size();
+        }
+        else
+        {
+            ccSizes.second += staIds.size();
+        }
         for (std::size_t i = 0; i < numSubchannelsForRu; ++i)
         {
+            auto ruAllocNoUsers = HeRu::GetEqualizedRuAllocation(ruType, false, false);
             ruTypes.at(index + i) = ruType;
-            ruAllocations.at(index + i) = ruAlloc;
+            ruAllocations.at(index + i) =
+                (IsSigBCompression() || ((index + i) % 2) == ccIndex) ? ruAlloc : ruAllocNoUsers;
         }
     }
     return ruAllocations;