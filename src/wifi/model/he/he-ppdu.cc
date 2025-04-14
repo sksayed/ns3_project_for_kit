nsnam/ns-3-dev-git

wifi: Use HeRu::GetNRus directly in GetHeSigBContentChannels

src/wifi/model/he/he-ppdu.cc
@@ -712,9 +712,7 @@ HePpdu::GetHeSigBContentChannels(const WifiTxVector& txVector, uint8_t p20Index)
             continue;
         }
 
-        std::size_t numRus = (ruType >= HeRu::RU_242_TONE)
-                                 ? 1
-                                 : HeRu::m_heRuSubcarrierGroups.at({MHz_t{20}, ruType}).size();
+        const auto numRus = HeRu::GetNRus(MHz_t{20}, ruType);
         auto ruIdx = ru.GetIndex();
         while (prevRuIndex < ruIndex - 1)
         {

sksayed/ns3_project_for_kit

src/wifi/model/he/he-ppdu.cc
@@ -702,9 +702,7 @@ HePpdu::GetHeSigBContentChannels(const WifiTxVector& txVector, uint8_t p20Index)
             continue;
         }
 
-        std::size_t numRus = (ruType >= HeRu::RU_242_TONE)
-                                 ? 1
-                                 : HeRu::m_heRuSubcarrierGroups.at({MHz_u{20}, ruType}).size();
+        const auto numRus = HeRu::GetNRus(MHz_u{20}, ruType);
         auto ruIdx = ru.GetIndex();
         while (prevRuIndex < ruIndex - 1)
         {